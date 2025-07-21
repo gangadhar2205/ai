a1b6ebad3357e2bb94214ad7fe40d7a150d20bd6
gangadhar2205 <gangadharkks@gmail.com>
2025-07-21 10:24:31 +0530

    deploying the things

---
+++ src/main/java/com/mortgages/ai/mortgageservices/controller/AipController.java
@@ -0,0 +1,10 @@
+package com.mortgages.ai.mortgageservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AipController {
+
+}
